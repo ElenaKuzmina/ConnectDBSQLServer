8bcd283d6fa745783c952d8d424b1bfbf7ff4399
Elena Kuzmina <kashtan.kuzmin@gmail.com>
2022-09-23 12:10:02 +0300

    Добавьте файлы проекта.

---
+++ Pages/PageAddEdit.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ConnectDBSQLServer.Classes;
+
+namespace ConnectDBSQLServer.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddEdit.xaml
+    /// </summary>
+    public partial class PageAddEdit : Page
+    {
+        //новое поле, которое будет хранить в себе экземпляр добавляемого пользователя
+
+        private User _currentUser = new User();
+
+        public PageAddEdit()
+        {//создаем контекст
+            DataContext = _currentUser;
+
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder error = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentUser.FirstName))
+                error.AppendLine("Укажите имя");
+            if (string.IsNullOrWhiteSpace(_currentUser.LastName))
+                error.AppendLine("Укажите фамилию");
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+            dbISP19AEntities.GetContext().User.Add(_currentUser);
+            try
+            {
+                dbISP19AEntities.GetContext().SaveChanges();
+                MessageBox.Show("Новый пользователь добавлен");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Pages/PageUser.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ConnectDBSQLServer.Classes;
+
+namespace ConnectDBSQLServer.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageUser.xaml
+    /// </summary>
+    public partial class PageUser : Page
+    {
+        public PageUser()
+        {
+            InitializeComponent();
+           DGridUsers.ItemsSource = dbISP19AEntities.GetContext().User.ToList();
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.frmObj.Navigate(new PageAddEdit());
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.frmObj.Navigate(new PageAddEdit());
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                dbISP19AEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridUsers.ItemsSource = dbISP19AEntities.GetContext().User.ToList();
+            }
+        }
+    }
+}

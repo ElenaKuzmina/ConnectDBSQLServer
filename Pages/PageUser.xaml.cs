e4047de59d4143775a52de366d6aa0668760537e
Elena Kuzmina <kashtan.kuzmin@gmail.com>
2022-10-28 10:38:16 +0300

    Вывод в Excel

--- Pages/PageUser.xaml.cs
+++ Pages/PageUser.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ConnectDBSQLServer.Classes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ConnectDBSQLServer.Pages
 {
@@ -21,14 +22,19 @@ namespace ConnectDBSQLServer.Pages
     /// </summary>
     public partial class PageUser : Page
     {
+       
+
         public PageUser()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            // получение списка данных
+            
            DGridUsers.ItemsSource = dbISP19AEntities.GetContext().User.ToList();
 
             CmbFiltrLogin.ItemsSource = dbISP19AEntities.GetContext().Account.ToList();
             CmbFiltrLogin.SelectedValuePath = "ID";
             CmbFiltrLogin.DisplayMemberPath = "Login";
+       
            
         }
 
@@ -76,6 +82,7 @@ namespace ConnectDBSQLServer.Pages
         private void CmbFiltrLogin_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {//фильтр по логину
             int id = Convert.ToInt32(CmbFiltrLogin.SelectedValue);
+            
             DGridUsers.ItemsSource = dbISP19AEntities.GetContext().User.Where(x=>x.Login_ID == id).ToList();
         }
 
@@ -87,6 +94,41 @@ namespace ConnectDBSQLServer.Pages
         private void BtnToList_Click(object sender, RoutedEventArgs e)
         {
             ClassFrame.frmObj.Navigate(new PageListUser());
+        }
+
+        private void BtnExcel_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            //объект Excel
+            var app = new Excel.Application();
+
+            //книга 
+            Excel.Workbook wb = app.Workbooks.Add();
+            //лист
+            Excel.Worksheet worksheet = app.Worksheets.Item[1];
+            int indexRows = 1;
+            //ячейка
+            worksheet.Cells[1][indexRows] = "Номер";
+            worksheet.Cells[2][indexRows] = "Фамилия";
+            worksheet.Cells[3][indexRows] = "Имя";
+            worksheet.Cells[4][indexRows] = "Адрес";
+            worksheet.Cells[5][indexRows] = "Телефон";
+            var printItems = dbISP19AEntities.GetContext().User.ToList();
+            //цикл по данным из таблицы
+            foreach (var item in printItems)
+            {
+                worksheet.Cells[1][indexRows + 1] = indexRows;
+                worksheet.Cells[2][indexRows + 1] = item.LastName;
+                worksheet.Cells[3][indexRows + 1] = item.FirstName;
+                worksheet.Cells[4][indexRows + 1] = item.Adress;
+                worksheet.Cells[5][indexRows + 1] = item.Phone.ToString();
+                indexRows++;
+            }
+
+            //показать Excel
+            app.Visible = true;
+            
         }
     }
 }
